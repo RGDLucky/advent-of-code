ed876fc8d37a0e2372d11c8ef242a1d9e91b0d71
RGDLucky <ryan.dinville@gmail.com>
2023-12-02 09:52:11 -0700

    day 1 part 2 finished

--- 2023/day-01/part2.rs
+++ 2023/day-01/part2.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
-    let file = File::open("input1.txt")?;
+    let file = File::open("input2.txt")?;
     let reader = BufReader::new(file);
     let mut result = 0;
 
@@ -18,14 +18,47 @@ fn main() -> io::Result<()> {
 } 
 
 fn get_number(line: String) -> i32 {
-    let numbers = { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" }
-    // check each char in line to see if it is a number and the first one store it
-    // store the last one until you run out of characters
-    for i in 0..line.len {
-
+    let numbers = [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+    let mut digit1 = -1;
+    let mut digit2 = -1;
+    for i in 0..line.len() {
+        let mut count = 1;
+        if check_digit(line.chars().nth(i).unwrap()) {
+            if digit1 == -1 {
+                digit1 = line.chars().nth(i).unwrap() as i32 - 48;
+            }
+            digit2 = line.chars().nth(i).unwrap() as i32 - 48;
+            continue;
+        }
+        for number in numbers {
+            if check_number(line.clone(), number.to_string(), i) {
+                if digit1 == -1 {
+                    digit1 = count;
+                }
+                digit2 = count;
+            }
+            count += 1;
+        }
     }
+    println!("{} {}", digit1, digit2);
+    digit1 * 10 + digit2
 }
 
-fn check_number(line: String, number: String, start: i32) {
-    
+fn check_number(line: String, number: String, start: usize) -> bool {
+    let mut line_index = start;
+    for character in number.chars() {
+        if line_index >= line.len() || line.chars().nth(line_index).unwrap() != character {
+            return false;
+        }
+        line_index += 1;
+    }
+
+    true
+}
+
+fn check_digit(character: char) -> bool {
+    match character {
+        '1' | '2' | '3' | '4' | '5' | '6'| '7' | '8' | '9' => return true,
+        _ => return false,
+    }
 }
