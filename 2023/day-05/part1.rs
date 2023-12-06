731f46a7833960275dad0e5bfc767fa6d17bf8f2
RGDLucky <ryan.dinville@gmail.com>
2023-12-06 07:49:17 -0700

    day 5 part 1 made a solutiong to get from seed to fertilizer and a bit of a template for the rest

--- 2023/day-05/part1.rs
+++ 2023/day-05/part1.rs
@@ -13,31 +13,38 @@ fn main() -> io::Result<()> {
     let map: Vec<String> = reader.lines().map(|l| l.expect("Could not parse line"))
         .collect();
     let seed_line: Vec<&str> = map[0].split(": ").collect();
-    let mut seeds: Vec<&str> = seed_line[1].split(" ").collect();
+    let seeds: Vec<&str> = seed_line[1].split(" ").collect();
     let mut index = 3;
 
     // use hash map to get value seed to soil
     let mut seed_to_soil = HashMap::new();
     while index < map.len() {
-        if map[index] = "" { break; }
+        if map[index] == "" { break; }
         let convertion: Vec<&str> = map[index].split(" ").collect();
-        let seed = convertion[1].parse::<i32>();
-        let soil = convertion[0].parse::<i32>();
+        let mut seed = convertion[1].parse::<i32>().unwrap();
+        let mut soil = convertion[0].parse::<i32>().unwrap();
 
-        for i in 0..convertion[2].parse::<i32>() {
+        for i in 0..convertion[2].parse::<i32>().unwrap() {
             seed += i;
-            soil += seed;
+            soil += i;
             seed_to_soil.insert(seed, soil);
         }
         index += 1;
     }
 
+    let mut soils: Vec<i32> = vec![];
+
     for i in 0..seeds.len() {
-        seeds[i] = seed_to_soil[seeds[i]];
+        let seed = seeds[i].parse::<i32>().unwrap(); 
+        if seed_to_soil.contains_key(&seed) {
+            soils.push(seed_to_soil[&seed]);
+        } else {
+            soils.push(seed);
+        }
     }
 
     // use hash map to get value seed to soil
-    let mut soil_to_fertilizer = HashMap::new();
+    //let mut soil_to_fertilizer = HashMap::new();
     while index < map.len() {
 
         index += 1;
