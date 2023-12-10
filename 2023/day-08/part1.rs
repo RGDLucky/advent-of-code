3babec230a9e553642b653c366922f4fc5e7175c
RGDLucky <ryan.dinville@gmail.com>
2023-12-09 17:40:56 -0700

    day 8 part 1 directed graph does not work as well as I thought

--- 2023/day-08/part1.rs
+++ 2023/day-08/part1.rs
@@ -4,17 +4,25 @@ use std::collections::HashMap;
 
 // Notes/thoughts: create a directed graph data structure, put all Vertexes and Indices into the the graph
 // use BFS to find the the shortest path
+//
+// Store all lines into a vec then when you need use directions to find zzz
 
 fn main() -> io::Result<()> {
     let file = File::open("input1.txt")?;
     let reader = BufReader::new(file);
     let mut result = 0;
+    let graph = DirectedGraph::new();
 
     for line in reader.lines() {
-        println!("TODO");
+        let new_line = line.unwrap();
+        let nodes: Vec<&str> = new_line.split(" = ").collect();
+        let root = nodes[0];
+        let children = nodes[1].split(", ").collect();
+        graph.addEdge(root.clone(), children[0]);
+        graph.addEdge(root, children[1]);
     }
 
-    println!("{}", result);
+    println!("{}", );
 
     Ok(())
 }   
@@ -25,7 +33,7 @@ fn main() -> io::Result<()> {
 struct DirectedGraph {
     V: i32,
     E: i32,
-    adj: HashMap<i32, Vec<i32>>,
+    adj: HashMap<&str, Vec<&str>>,
 }
 
 // Directed Graph Methods
@@ -36,14 +44,18 @@ impl DirectedGraph {
         self.E = 0;
     }
 
-    fn addEdge(&mut self, v: i32, w: i32) {
-        if self.adj.contains_key(&v) { self.adj[&v].push(w); }
+    fn addEdge(&mut self, v: &str, w: &str) {
+        if self.adj.contains_key(&v) { 
+            let mut new_vec: Vec<&str> = self.adj[&v].clone();
+            new_vec.push(w); 
+            self.adj.insert(v, new_vec);
+        }
         else {
             self.adj.insert(v, vec![w]);
         }
     }
     
-    fn find_shortest_path(&self, v: i32, w: i32) {
+    fn find_shortest_path(&self, v: &str, w: &str) {
 
     }
 }
