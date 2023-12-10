6f2679b1449f7b7bcfc879e9eaff74dbce5e00e6
RGDLucky <ryan.dinville@gmail.com>
2023-12-10 15:56:34 -0700

    day 8 part 1 completed

--- 2023/day-08/part1.rs
+++ 2023/day-08/part1.rs
@@ -2,27 +2,45 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use std::collections::HashMap;
 
-// Notes/thoughts: create a directed graph data structure, put all Vertexes and Indices into the the graph
-// use BFS to find the the shortest path
-//
-// Store all lines into a vec then when you need use directions to find zzz
-
 fn main() -> io::Result<()> {
     let file = File::open("input1.txt")?;
     let reader = BufReader::new(file);
     let mut result = 0;
-    let graph = DirectedGraph::new();
+    let mut graph = DirectedGraph::new();
+    let mut directions: String = String::from("");
+    let mut contents: Vec<String> = vec![];
+    
+    for line in reader.lines() { contents.push(line.unwrap()); }
 
-    for line in reader.lines() {
-        let new_line = line.unwrap();
-        let nodes: Vec<&str> = new_line.split(" = ").collect();
-        let root = nodes[0];
-        let children = nodes[1].split(", ").collect();
-        graph.addEdge(root.clone(), children[0]);
-        graph.addEdge(root, children[1]);
+    let mut index = 0;
+    while index < contents.len() {
+        if contents[index] == "" { index += 1; break; }
+        directions = directions + &contents[index];
+        index += 1;
     }
 
-    println!("{}", );
+     while index < contents.len() {
+        let nodes: Vec<&str> = contents[index].split(" = ").collect();
+        let root = nodes[0];
+        let children: Vec<&str> = nodes[1].split(", ").collect();
+        graph.add_vertex(root, &children[0][1..], &children[1][..3]);
+        index += 1;
+    }
+
+    let mut curr = "AAA";
+    let mut found = false;
+
+    loop {
+        for direction in directions.chars() {
+            if curr == "ZZZ" { found = true; break; }
+            result += 1;
+            curr = graph.get_child(direction.clone(), curr);
+        }
+
+        if found { break; }
+    }
+
+    println!("{}", result);
 
     Ok(())
 }   
@@ -30,32 +48,31 @@ fn main() -> io::Result<()> {
 
 
 // Directed Graph Data Structure
-struct DirectedGraph {
-    V: i32,
-    E: i32,
-    adj: HashMap<&str, Vec<&str>>,
+struct DirectedGraph<'a> {
+    v: i32,
+    e: i32,
+    adj: HashMap<&'a str, Vec<&'a str>>,
 }
 
 // Directed Graph Methods
-impl DirectedGraph {
-    fn new(&mut self) {
-        self.adj = HashMap::new();
-        self.V = 0;
-        self.E = 0;
-    }
-
-    fn addEdge(&mut self, v: &str, w: &str) {
-        if self.adj.contains_key(&v) { 
-            let mut new_vec: Vec<&str> = self.adj[&v].clone();
-            new_vec.push(w); 
-            self.adj.insert(v, new_vec);
-        }
-        else {
-            self.adj.insert(v, vec![w]);
+impl<'a> DirectedGraph<'a> {
+    fn new() -> Self {
+        Self { 
+            v: 0,
+            e: 0,
+            adj: HashMap::new(),
         }
     }
-    
-    fn find_shortest_path(&self, v: &str, w: &str) {
 
+    fn add_vertex(&mut self, v: &'a str, child1: &'a str, child2: &'a str) {
+        self.adj.insert(v, vec![child1, child2]);
+        self.v += 1;
+        self.e += 1;
+    }
+
+    fn get_child(&self, direction: char, node: &str) -> &str {
+        let curr_adj = &self.adj[node];
+        if direction == 'R' { return curr_adj[1]; }
+        return curr_adj[0];
     }
 }
