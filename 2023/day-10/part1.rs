c49adcdf393417c805fc228c728da22e710c94f1
RGDLucky <ryan.dinville@gmail.com>
2023-12-12 21:02:43 -0700

    day 10 part 1 completed

--- 2023/day-10/part1.rs
+++ 2023/day-10/part1.rs
@@ -1,20 +1,13 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
-// Ideas/Thought: add periods to buffer the top bottom left and right. put everything in 2d vector
-// of chars and find the S position and then use bfs to find a loop.  Stoping point in bfs is when
-// it runs into another digit then get that digit and return it
-//
-// New Idea: use dfs that when loops back to S prints count / 0, make a dfs structure with methods,
-// use an enum for direction, each iteration will have a direction dictating what to look for
-
 fn main() -> io::Result<()> {
     let file = File::open("input1.txt")?;
     let reader = BufReader::new(file);
-    let mut result = 0;
     let mut field: Vec<Vec<char>> = vec![];
     let mut buffer: Vec<char> = vec![];
-    for i in 0..7 { buffer.push('.'); }
+    for _i in 0..7 { buffer.push('.'); }
+    field.push(buffer.clone());
 
     let mut sx = 0;
     let mut sy = 0;
@@ -25,11 +18,11 @@ fn main() -> io::Result<()> {
         let line_chars: Vec<char> = line?.chars().collect();
         let mut new_vec: Vec<char> = vec![];
         new_vec.push('.');
-        for _i in 0..line_chars.len() {
+        for i in 0..line_chars.len() {
             new_vec.push(line_chars[i]);
             if line_chars[i] == 'S' {
-                sx = i;
-                sy = line_index;
+                sx = line_index + 1;
+                sy = i + 1;
             }
         }
 
@@ -37,93 +30,67 @@ fn main() -> io::Result<()> {
         field.push(new_vec);
         line_index += 1;
     }
-    /*
-    // BFS to find path
-    let level = 0;
-    let queue: Vec<Vec<usize>> = vec![];
-    let start_index: Vec<usize> = vec![sx, sy, level];
-    level += 1;
-    // check all sides of S for valid directions and add them to the queue
-    // north
-    if field[sx][sy+1] == '|' || field[sx][sy+1] == '7' || field[sx][sy+1] == 'F' { queue.push(vec![sx, sy+1, level]);
-    // south
-    if field[sx][sy-1] == '|' || field[sx][sy-1] == 'L' || field[sx][sy-1] == 'J' { queue.push(vec![sx, sy-1, level]);
-    // east
-    if field[sx-1][sy] == '-' || field[sx-1][sy] == 'F' || field[sx-1][sy] == 'L' { queue.push(vec![sx-1], sy, level]);
-    // west
-    if field[sx+1][sy] == '-' || field[sx+1][sy] == 'J' || field[sx+1][sy] == '7' { queue.push(vec![sx-1, sy, level]);
-    loop {
-        let (x, y) = queue[0]; 
-        queue.remove(0);
-        level += 1;
+    field.push(buffer);
 
-    }
-*/
     let dfs = DFS::new(field, sx, sy);
-    result = dfs.search();
+    let result = dfs.search();
 
-    // println!("S at: {} {}", sx, sy);
-    //for line in field { println!("{}", line ); }
-
-    println!("{}", result);
+    println!("{}", result / 4);
 
     Ok(())
-}    
+}
+
+#[derive(PartialEq)]
+enum Direction {
+    Up,
+    Down,
+    Right,
+    Left,
+    DeadEnd,
+}
 
 struct DFS {
-    feild: Vec<Vec<char>>,
+    field: Vec<Vec<char>>,
     sr: usize,
     sc: usize,
 }
 
 impl DFS {
-    enum Direction {
-        Up,
-        Down,
-        Right,
-        Left,
-        DeadEnd,
-    }
 
     fn new(new_field: Vec<Vec<char>>, new_sr: usize, new_sc: usize) -> Self {
         Self {
-            field: new_field
+            field: new_field,
             sr: new_sr,
             sc: new_sc,
         }
     }
 
-    // kick things off by checking each direction starting from S then calls searchHelper for each
-    // of them
     fn search(&self) -> usize {
-        let up = checkUp(field[sr+1][sc]);
-        let down = checkDown(field[sr - 1][sc]);
-        let right = checkDown(field[sr][sc + 1]);
-        let left = checkLeft(field[sr][sc - 1]);
+        let up = Self::check_up(self.field[self.sr - 1][self.sc]);
+        let down = Self::check_down(self.field[self.sr + 1][self.sc]);
+        let right = Self::check_right(self.field[self.sr][self.sc + 1]);
+        let left = Self::check_left(self.field[self.sr][self.sc - 1]);
         
-        return searchHelper(sr + 1, sc, up, 1) + searchHelper(sr - 1, sc, down, 1) + searchHelper(sr, sc + 1, right, 1) + searchHelper(sr, sc - 1, left, 1);
+        return self.search_helper(self.sr - 1, self.sc, up, 1) + self.search_helper(self.sr + 1, self.sc, down, 1) + self.search_helper(self.sr, self.sc + 1, right, 1) + self.search_helper(self.sr, self.sc - 1, left, 1);
     }
 
-    // Recursion that keeps going through path until it hits S or an invalid pipe
-    // also keeps a counter
-    fn searchHelper(row: usize, col: usize, direction: Direction, count: usize) -> usize {
-        if field[row][col] == 'S' { return count; }
+    fn search_helper(&self, row: usize, col: usize, direction: Direction, count: usize) -> usize {
+        if self.field[row][col] == 'S' { return count; }
         if direction == Direction::DeadEnd { return 0; }
-        let new_row = row;
-        let new_col = col;
+        let mut new_row = row;
+        let mut new_col = col;
         let new_direction;
         match direction {
-            Direction::Up => { new_row = row + 1; new_direction = checkUp(field[new_row][new_col]); },
-            Direction::Down => { new_row = row - 1; new_direction = checkDown(field[new_row][new_col]); },
-            Direction::Right => { new_col = col + 1; new_direction = checkRight(field[new_row][new_col]); },
-            Direction::Left => { new_col = col - 1; new_direction = checkLeft(field[new_row][new_col]); },
+            Direction::Up => { new_row = row - 1; new_direction = Self::check_up(self.field[new_row][new_col]); },
+            Direction::Down => { new_row = row + 1; new_direction = Self::check_down(self.field[new_row][new_col]); },
+            Direction::Right => { new_col = col + 1; new_direction = Self::check_right(self.field[new_row][new_col]); },
+            Direction::Left => { new_col = col - 1; new_direction = Self::check_left(self.field[new_row][new_col]); },
+            Direction::DeadEnd => panic!(),
         }
-        return searchHelper(new_row, new_col, new_direction, count + 1); 
+        return self.search_helper(new_row, new_col, new_direction, count + 1); 
     }
 
-    // if previous pipe went up check if the next one is valid and return its direction or say its
-    // a dead end
-    fn checkUp(pipe: char) -> Direction {
+    fn check_up(pipe: char) -> Direction {
         match pipe {
             '|' => return Direction::Up,
             '7' => return Direction::Left,
@@ -132,7 +99,7 @@ impl DFS {
         }
     }
     
-    fn checkDown(pipe: char) -> Direction {
+    fn check_down(pipe: char) -> Direction {
         match pipe {
             '|' => return Direction::Down,
             'L' => return Direction::Right,
@@ -141,7 +108,7 @@ impl DFS {
         }
     }
 
-    fn checkRight(pipe: char) -> Direction {
+    fn check_right(pipe: char) -> Direction {
         match pipe {
             '-' => return Direction::Right,
             '7' => return Direction::Down,
@@ -150,7 +117,7 @@ impl DFS {
         }
     }
 
-    fn checkLeft(pipe: char) -> Direction {
+    fn check_left(pipe: char) -> Direction {
         match pipe {
             '-' => return Direction::Left,
             'F' => return Direction::Down,
