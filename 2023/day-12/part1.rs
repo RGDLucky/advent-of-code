27357c44bcecf6a534aad9129e6695c4ebf036d8
RGDLucky <ryan.dinville@gmail.com>
2023-12-13 17:23:52 -0700

    day 12 part 2 started it and layed out basic parsing but have not figured out a way to find all possible combinations

---
+++ 2023/day-12/part1.rs
@@ -0,0 +1,29 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+// Notes/Ideas: Split each line by " " so that you can get the numbers of each spring,
+// iterate through row and update position in numbers so you can find ? and then find
+// possible arrangements
+
+fn main() -> io::Result<()> {
+    let file = File::open("input1.txt")?;
+    let reader = BufReader::new(file);
+    let mut result = 0;
+
+    for line in reader.lines() {
+        let new_line = line.unwrap();
+        let contents: Vec<&str> = new_line.split(" ").collect();
+        let conditions: Vec<char> = contents[0].chars();
+        let sizes: Vec<&str> = contents.split(",").collect();
+        let index = 0;
+
+        // iterate through conditions and find all possible solutions for ????
+        for i in 0..conditions.len() {
+
+        }
+    }
+
+    println!("{}", result);
+
+    Ok(())
+} 
