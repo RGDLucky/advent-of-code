17cccce3572011185284e2b89434aa9226b19e83
RGDLucky <ryan.dinville@gmail.com>
2023-12-15 13:16:44 -0700

    day 12 part 1 completed

--- 2023/day-12/part1.rs
+++ 2023/day-12/part1.rs
@@ -1,11 +1,6 @@
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
-// Notes/Ideas: Split each line by " " so that you can get the numbers of each spring,
-// iterate through row and update position in numbers so you can find ? and then find
-// possible arrangements
-// find possible arrangements by trying all possible combinations
-
 fn main() -> io::Result<()> {
     let file = File::open("input1.txt")?;
     let reader = BufReader::new(file);
@@ -14,14 +9,11 @@ fn main() -> io::Result<()> {
     for line in reader.lines() {
         let new_line = line.unwrap();
         let contents: Vec<&str> = new_line.split(" ").collect();
-        let conditions: Vec<char> = contents[0].chars();
+        let conditions: Vec<char> = contents[0].chars().collect();
         let sizes: Vec<&str> = contents[1].split(",").collect();
-        let index = 0;
-
-        // iterate through conditions and find all possible solutions for ????
-        for i in 0..conditions.len() {
-
-        }
+        
+        let combos: Vec<String> = get_combinations(0, conditions);
+        result += check_combinations(combos, sizes);
     }
 
     println!("{}", result);
@@ -29,17 +21,20 @@ fn main() -> io::Result<()> {
     Ok(())
 } 
 
-// recursive function that first raverse until the end of the line then constructs a vec that is
-// added to with each possible combination to each element in the vec
-fn get_combinations(index: usize, Vec<char> line) -> Vec<String> {
+fn get_combinations(index: usize, line: Vec<char>) -> Vec<String> {
     if index >= line.len() { return vec![String::from("")]; }
-    let combos: Vec<String> = get_combinations(index + 1, line);
-    if line[index] == '#' || line[index] == '.' { return combos: }
+    let combos: Vec<String> = get_combinations(index + 1, line.clone());
 
-    let new_combos: Vec<String> = vec![];
-    for i in 0..combos.len() {
-        new_combos.push("#" + combos[i]);
-        new_combos.push("." + combos[i]);
+    let mut new_combos: Vec<String> = vec![];
+    if line[index] == '#' || line[index] == '.' { 
+        for i in 0..combos.len() { new_combos.push(String::from(line[index]) + &combos[i]); }
+    }
+
+    else {
+        for i in 0..combos.len() {
+            new_combos.push(String::from("#") + &combos[i]);
+            new_combos.push(String::from(".") + &combos[i]);
+        }
     }
 
     return new_combos;
@@ -48,14 +43,17 @@ fn get_combinations(index: usize, Vec<char> line) -> Vec<String> {
 fn check_combinations(combos: Vec<String>, nums: Vec<&str>) -> usize {
     let mut result = 0;
     for i in 0..combos.len() {
-        let line: Vec<&str> = combos[i].split(".");
+        let line: Vec<&str> = combos[i].split(".").collect();
         let mut num_index = 0;
         let mut is_valid = 1;
         for i in 0..line.len() {
             if line[i] == "" { continue; }
-            if line[i].len() != nums[num_index].parse::<i32>().unwrap() { is_valid = 0; break; }
+            if num_index >= nums.len() || line[i].len() != nums[num_index].parse::<usize>().unwrap() { is_valid = 0; break; }
             num_index += 1;
         }
+
+        if num_index < nums.len() { is_valid = 0; }
         result += is_valid;
     }
+    return result;
 }
