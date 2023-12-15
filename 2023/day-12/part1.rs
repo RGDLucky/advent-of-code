54eb3c5666015e4991002a191984192edfc8a5d5
RGDLucky <ryan.dinville@gmail.com>
2023-12-15 12:45:37 -0700

    day 12 part 1 created twofunctions that generate all possible combinations and another function that checks how many valid combinations there are

--- 2023/day-12/part1.rs
+++ 2023/day-12/part1.rs
@@ -28,3 +28,34 @@ fn main() -> io::Result<()> {
 
     Ok(())
 } 
+
+// recursive function that first raverse until the end of the line then constructs a vec that is
+// added to with each possible combination to each element in the vec
+fn get_combinations(index: usize, Vec<char> line) -> Vec<String> {
+    if index >= line.len() { return vec![String::from("")]; }
+    let combos: Vec<String> = get_combinations(index + 1, line);
+    if line[index] == '#' || line[index] == '.' { return combos: }
+
+    let new_combos: Vec<String> = vec![];
+    for i in 0..combos.len() {
+        new_combos.push("#" + combos[i]);
+        new_combos.push("." + combos[i]);
+    }
+
+    return new_combos;
+}
+
+fn check_combinations(combos: Vec<String>, nums: Vec<&str>) -> usize {
+    let mut result = 0;
+    for i in 0..combos.len() {
+        let line: Vec<&str> = combos[i].split(".");
+        let mut num_index = 0;
+        let mut is_valid = 1;
+        for i in 0..line.len() {
+            if line[i] == "" { continue; }
+            if line[i].len() != nums[num_index].parse::<i32>().unwrap() { is_valid = 0; break; }
+            num_index += 1;
+        }
+        result += is_valid;
+    }
+}
