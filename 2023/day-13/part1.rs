ae48c62584e37d1d0795d0c8d2f08f07453b2013
RGDLucky <ryan.dinville@gmail.com>
2023-12-16 17:08:14 -0700

    Day 13 part 1 set up file reading and layed out functions to check vertical and horizontal symetry

--- 2023/day-13/part1.rs
+++ 2023/day-13/part1.rs
@@ -8,13 +8,22 @@ fn main() -> io::Result<()> {
     let file = File::open("input1.txt")?;
     let reader = BufReader::new(file);
     let mut result = 0;
-    let mut contents: Vec<Vec<char>> = vec![];
+    let mut contents: Vec<String> = vec![];
 
     for line in reader.lines() {
-        contents.push(line.chars().collect());
+        contents.push(line.unwrap());
     }
 
-    let vertical = check_vertical(contents.clone());
+    let mut index = 0;
+    while index < contents.len() {
+        let mut grid: Vec<Vec<char>> = vec![];
+        while contents[index] != "" {
+            grid.push(contents[index].chars().collect());
+            index += 1;
+        }
+        result += check_vertical(grid()) + check_horizontal(grid.clone());
+        index += 1;
+    }
 
     println!("{}", result);
 
